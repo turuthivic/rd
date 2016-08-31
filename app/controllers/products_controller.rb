dbdbbf99be888ecd6b60d9a971cc4057928fdf38
turuthivic <turuthivictor9@gmail.com>
2016-08-31 21:40:50 +0300

    added categories

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -14,18 +14,21 @@ class ProductsController < ApplicationController
 
   # GET /products/new
   def new
-    @product = Product.new
+    @product = Product.new 
+    @categories = Category.all.map { |c| [c.name, c.id]}
   end
 
   # GET /products/1/edit
   def edit
+    @categories = Category.all.map { |c| [c.name,c.id]  }
   end
 
   # POST /products
   # POST /products.json
   def create
     @product = Product.new(product_params)
-
+    @product.category_id = params[:category_id]
+    
     respond_to do |format|
       if @product.save
         format.html { redirect_to @product, notice: 'Product was successfully created.' }
@@ -69,6 +72,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:title, :description, :category_id, :price)
+      params.require(:product).permit(:title, :description, :category_id, :price, :image)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160831132824) do
+ActiveRecord::Schema.define(version: 20160831134859) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
@@ -23,6 +30,7 @@ ActiveRecord::Schema.define(version: 20160831132824) do
     t.string   "price"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "image"
   end
 
 end
