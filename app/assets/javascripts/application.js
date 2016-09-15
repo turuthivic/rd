b9bf9f91ba547e1cb85a8a6e3f5e68e8aa25a3ac
turuthivic <turuthivictor9@gmail.com>
2016-09-15 13:49:25 +0300

    nprogress rails

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,8 @@
 // about supported directives.
 //
 //= require jquery
+//= require nprogress
+//= require nprogress-turbolinks
 //= require bootstrap
 //= require jquery_ujs
 //= require turbolinks
