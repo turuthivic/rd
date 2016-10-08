dec17db276f0ff1acb35d77b8e1a61362a1f3571
turuthivic <turuthivictor9@gmail.com>
2016-10-08 15:59:24 +0300

    final commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160921052245) do
+ActiveRecord::Schema.define(version: 20160926094245) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,10 @@ ActiveRecord::Schema.define(version: 20160921052245) do
   create_table "contacts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "name"
+    t.string   "message"
+    t.string   "email"
+    t.string   "nickname"
   end
 
   create_table "products", force: :cascade do |t|
