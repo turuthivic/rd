51116fe4b93ad64ea41b63317d50a5812698f544
turuthivic <turuthivictor9@gmail.com>
2016-10-24 15:44:44 +0300

    categories auto-update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :set_categories
   before_filter :authenticate_admin!
   # before_filter :authenticate_user!
+
+  private
+
+  def set_categories
+  	@categories = Category.where(@category_id)
+  end
 end
