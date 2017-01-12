1daa47182ff5954eac1f499da2fb7530abbcbc5b
turuthivic <turuthivictor9@gmail.com>
2017-01-12 13:28:29 +0300

    changed ip on production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,8 +2,8 @@
 # ======================
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
-server '172.31.42.82', user: 'deploy', roles: %w{web app db}
-
+server '172.31.12.137', user: 'deploy', roles: %w{web app db}
+config.assets.prefix = "/shared/public/assets"
 # server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
